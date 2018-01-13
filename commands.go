0442bbea7baef925e32cfdad16c5ffa590af1e1b
Michael (Parker) Parker <parkervcp@gmail.com>
2018-01-12 23:16:47 -0500

    adding tesseract-ocr support

--- commands.go
+++ commands.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/otiai10/gosseract"
+	"mvdan.cc/xurls"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -38,6 +43,46 @@ func hasPrefix(a string) bool {
 	return strings.HasPrefix(a, getConfig("prefix"))
 }
 
+func parseImage(remoteURL string) string {
+	log.Info("Reading from " + remoteURL)
+
+	remote, e := http.Get(remoteURL)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	defer remote.Body.Close()
+	lastBin := strings.LastIndex(remoteURL, "/")
+	fileName := remoteURL[lastBin+1:]
+
+	log.Info("Filename is " + fileName)
+
+	//open a file for writing
+	file, err := os.Create("/tmp/" + fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Use io.Copy to just dump the response body to the file. This supports huge files
+	_, err = io.Copy(file, remote.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.Close()
+	log.Info("Image File Pulled and saved to /tmp/" + fileName)
+
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	client.SetImage("/tmp/" + fileName)
+	text, err := client.Text()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return text
+}
+
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -64,6 +109,10 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// If the prefix is not present
 	if hasPrefix(input) == false {
+		if strings.Contains(strings.ToLower(input), ".png") == true {
+			remoteURL := xurls.Relaxed().FindString(input)
+			input = parseImage(remoteURL)
+		}
 		for _, p := range commands {
 			if strings.Contains(strings.ToLower(input), p.Cmd) {
 				if p.Typ == "listen" {
@@ -81,20 +130,18 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if strings.Contains(input, "ggl") {
 			response = "<https://lmgtfy.com/?q=" + strings.Replace(strings.TrimPrefix(input, "ggl "), " ", "+", -1) + ">"
 			return
-		} else {
-			//Search command file for command and prep response
-			for _, p := range commands {
-				if p.Cmd == strings.ToLower(strings.TrimPrefix(input, getConfig("prefix"))) {
-					if p.Typ == "chat" {
-						for _, line := range p.Lns {
-							response = response + "\n" + line
-							log.Info("Message Sent" + response)
-						}
-					} else if p.Typ == "group" {
-						if getAdmin(m.Author.ID) == false {
-						}
-						s.ChannelMessageSend(m.ChannelID, "You're an admin")
+		}
+		//Search command file for command and prep response
+		for _, p := range commands {
+			if p.Cmd == strings.ToLower(strings.TrimPrefix(input, getConfig("prefix"))) {
+				if p.Typ == "chat" {
+					for _, line := range p.Lns {
+						response = response + "\n" + line
 					}
+				} else if p.Typ == "group" {
+					if getAdmin(m.Author.ID) == false {
+					}
+					s.ChannelMessageSend(m.ChannelID, "You're an admin")
 				}
 			}
 		}
@@ -103,9 +150,9 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if response == "" {
-
+		return
 	} else {
-		log.Info("Message Sent" + response)
+		log.Info("Message Sent" + "\n" + response)
 		s.ChannelMessageSend(m.ChannelID, response)
 	}
 }
