bd9fdb26088e269854f3699a00637afbaa429e37
Michael (Parker) Parker <parkervcp@gmail.com>
2018-04-16 09:33:25 -0400

    autoconfig update

--- configauto.go
+++ configauto.go
@@ -10,10 +10,9 @@ import (
 // configFilecheck to check if the files exist before starting up.
 func configFilecheck() bool {
 	if _, err := os.Stat("configs/"); err != nil {
+		// create configs folder
 		if os.IsNotExist(err) {
 			os.Mkdir("configs/", 0755)
-		} else {
-			// other error
 		}
 	} else {
 		writeLog("info", "Config folder exists", nil)
@@ -23,6 +22,11 @@ func configFilecheck() bool {
 		}
 		if checkConfigExists("discord.yml") == false {
 			writeLog("info", "Need to generate config for discord. Doing that now.", nil)
+			setupDiscordConfig()
+		}
+		if checkConfigExists("irc.yml") == false {
+			writeLog("info", "Need to generate config for irc. Doing that now.", nil)
+			setupIRCConfig()
 		}
 		return true
 	}
@@ -52,13 +56,12 @@ func askBoolQuestion(question string) bool {
 		ValidateFunc: func(in string) error {
 			if in == "Y" || in == "y" || in == "Yes" || in == "yes" {
 				answer = true
-				return nil
 			} else if in == "N" || in == "n" || in == "No" || in == "no" {
 				answer = false
-				return nil
 			} else {
 				return fmt.Errorf("Need to have a Y/n answer")
 			}
+			return nil
 		},
 	})
 	if err != nil {
@@ -68,24 +71,16 @@ func askBoolQuestion(question string) bool {
 	return answer
 }
 
-func askStingQuestion(question string) string {
-
+func askStringQuestion(question string) string {
 	ui := &input.UI{
 		Writer: os.Stdout,
 		Reader: os.Stdin,
 	}
 
 	answer, err := ui.Ask(question, &input.Options{
+		// Read the default val from env var
 		Required: true,
 		Loop:     true,
-		// Validate input
-		ValidateFunc: func(s string) error {
-			if s != "Y" && s != "n" {
-				writeLog("error", "", nil)
-			}
-
-			return nil
-		},
 	})
 	if err != nil {
 		writeLog("fatal", "", err)
@@ -97,33 +92,139 @@ func askStingQuestion(question string) string {
 func setupBotConfig() {
 	var services []string
 
-	discord := askBoolQuestion("Do you plan on supporting discord? [Y/n]")
-	irc := askBoolQuestion("Do you plan on supporting irc? [Y/n]")
-
-	switch {
-	case discord == true:
+	if askBoolQuestion("Do you plan on supporting discord? [Y/n]") == true {
 		services = append(services, "discord")
-	case irc == true:
+		writeLog("info", "Discord enabled", nil)
+		setupDiscordConfig()
+	}
+	if askBoolQuestion("Do you plan on supporting irc? [Y/n]") == true {
 		services = append(services, "irc")
+		writeLog("info", "IRC enabled", nil)
+		setupIRCConfig()
 	}
 
-	servicestring := ""
+	Bot.Set("bot.services", services)
 
-	for _, cr := range services {
-		servicestring = servicestring + cr + ", "
+	if askBoolQuestion("Do you want to use the default log location of 'logs/'? [Y/n]") == false {
+		Bot.Set("bot.log.location", askStringQuestion("Where you like logs to be saved?"))
+	} else {
+		Bot.Set("bot.log.location", "logs/")
 	}
 
-	writeLog("debug", "services loaded are "+servicestring, nil)
+	if askBoolQuestion("Do you want to use the default log level of info? [Y/n]") == false {
+		level := askStringQuestion("what log level would you like to use? [info/debug]")
+		if level == "info" || level == "debug" {
+			Bot.Set("bot.log.level", level)
+		} else {
+			writeLog("info", "Invalid log level set with "+level+". Defaulting to info", nil)
+			Bot.Set("bot.log.level", "info")
+		}
+
+	} else {
+		Bot.Set("bot.log.level", "info")
+	}
 
 	Bot.WriteConfigAs("configs/bot.yml")
 }
 
 func setupDiscordConfig() {
+	// get discord bot token
+	Discord.Set("discord.token", askStringQuestion("What is your discord token?"))
 
+	// set bot prefix
+	if askBoolQuestion("Would you like to use the default prefix? '.' [Y/n]") == false {
+		Discord.Set("discord.prefix", askStringQuestion("What is the prefix you'd like to use?"))
+	} else {
+		Discord.Set("discord.prefix", ".")
+	}
+
+	// set bot owner
+	if askBoolQuestion("Would you like to set a owner? (defaults to server owner) [Y/n]") == true {
+		Discord.Set("discord.owner", askStringQuestion("What is the server owners discord ID?"))
+	} else {
+		writeLog("info", "defaulting to server owner", nil)
+	}
+
+	var listening []string
+
+	// set channel filter up
+	if askBoolQuestion("Do you want the bot to listen on specific channels?") == true {
+		if askBoolQuestion("A channel to listen on it required. Would you like to set one now? [Y/n]") == true {
+			listening = append(listening, askStringQuestion("What is the ID of the channel you want to listen on?"))
+			Discord.Set("bot.channels.filter", true)
+			Discord.Set("bot.channels.listening", listening)
+			writeLog("info", "The channel is not verified will only work if correct.", nil)
+		} else {
+			Discord.Set("bot.channels.filter", false)
+			Discord.Set("bot.channels.listening", listening)
+		}
+	}
 }
 
 func setupIRCConfig() {
+	// get irc server settings
+	IRC.Set("irc.server.address", askStringQuestion("What is the server address"))
+	IRC.Set("irc.server.port", askStringQuestion("What is the server port?"))
+	IRC.Set("irc.server.ssl", askBoolQuestion("Does this connection require ssl [Y/n]"))
 
+	// get irc user settings
+	IRC.Set("irc.ident", askStringQuestion("What is the IRC username you are using?"))
+	IRC.Set("irc.email", askStringQuestion("What is the email associated with the account?"))
+	IRC.Set("irc.password", askStringQuestion("What is the password the bot is using?"))
+	IRC.Set("irc.nick", askStringQuestion("What is the nick that the bot should use?"))
+	IRC.Set("irc.real", askStringQuestion("What is the \"Real Name\" the bot should use?"))
+
+	// get irc text parsing settings
+	IRC.Set("irc.prefix", askStringQuestion("What is the prefix the bot should use?"))
+
+	// get irc channels
+
+	var listening []string
+
+	if askBoolQuestion("Do you want to add channels to join now? (you can pm the bot) [Y/n]") == true {
+		listening = append(listening, askStringQuestion("What channel is the bot supposed to join? (Without the # in the name)"))
+		for askBoolQuestion("Do you want to add more channels to join now? [Y/n]") == true {
+			listening = append(listening, askStringQuestion("What channel is the bot supposed to join? (Without the # in the name)"))
+		}
+	}
+
+	IRC.Set("irc.channels.listening", listening)
+
+	/*
+
+		This block is for future work. Namely permissions and other things.
+
+		// get irc groups
+
+		var admin []string
+		var mods []string
+		var blacklist []string
+
+		if askBoolQuestion("Do you want to add users to admin group now?? [Y/n]") == true {
+			admin = append(admin, askStringQuestion(""))
+			for askBoolQuestion("Do you want to add more groups to join now? [Y/n]") == true {
+				admin = append(admin, askStringQuestion(""))
+			}
+		}
+
+		if askBoolQuestion("Do you want to add users to mods group now?? [Y/n]") == true {
+			admin = append(mods, askStringQuestion(""))
+			for askBoolQuestion("Do you want add more mods now? [Y/n]") == true {
+				admin = append(mods, askStringQuestion(""))
+			}
+		}
+
+		if askBoolQuestion("Do you want to add users to admin group now?? [Y/n]") == true {
+			admin = append(blacklist, askStringQuestion(""))
+			for askBoolQuestion("Do you want blacklist more users now? [Y/n]") == true {
+				admin = append(blacklist, askStringQuestion(")"))
+			}
+		}
+
+		IRC.Set("irc.channels.groups.admin", admin)
+		IRC.Set("irc.channels.groups.mods", mods)
+		IRC.Set("irc.channels.groups.blacklist", blacklist)
+	*/
 }
 
 func setupCommandsConfig() {
