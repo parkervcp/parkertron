165d3cc7a71a22d47adcf8118af9dc24705747d6
Michael (Parker) Parker <parkervcp@gmail.com>
2018-05-08 08:58:37 -0400

    fixing startup bug

--- parkertron.go
+++ parkertron.go
@@ -73,8 +73,6 @@ func main() {
 	writeLog("debug", "Commands being loaded are: "+getCommandsString(), nil)
 	writeLog("debug", "Keywords being loaded are: "+getKeywordsString(), nil)
 
-	<-ServStat
-
 	writeLog("info", "Bot is now running.  Press CTRL-C to exit.\n", nil)
 	// Simple way to keep program running until CTRL-C is pressed.
 	<-make(chan struct{})
